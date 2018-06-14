5274b0db4ae0401fc08f7c9790de22080a98bfb7
ShushuY <31076051+shuhongyan@users.noreply.github.com>
2018-06-13 21:05:35 -0400

    06/13/2018 medium

---
+++ 81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 21:04:20 2018
+
+@author: shuhongyan
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        return target in nums
